eb0b7ab02ab02d86656dcc41cd47851f60929e10
Isaac Ortega <isaac.io.ortega@gmail.com>
2020-08-10 21:40:40 -0700

    Create React + AnimeJS demo

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -1,9 +1,20 @@
 import React from "react";
+import anime from "animejs";
 
 import "./Background.css";
 
-function Background() {
+const Background = () => {
+  React.useEffect(() => {
+    anime({
+      targets: ".background",
+      backgroundColor: "#FF0000",
+      easing: "easeInOutSine",
+      direction: "alternate",
+      loop: true,
+    });
+  }, []);
+
   return <div className="background"></div>;
-}
+};
 
 export default Background;
