618810210ecb0ce1358826796117d528dff540d1
Isaac Ortega <isaac.io.ortega@gmail.com>
2020-09-12 23:14:38 -0700

    Have menu rotate through warm colors

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import anime from "animejs";
 
 import MenuButton from "./MenuButton";
 
@@ -6,9 +7,28 @@ import "./Menu.css";
 
 import profilePic from "../../assets/profile.jpg";
 
+const menuColors = ["#A3403B", "#B3AA40", "#8740B3", "#AD8A3E"];
+const randomColor = () => {
+  return menuColors[Math.floor(Math.random() * menuColors.length)];
+};
+
 const Menu = () => {
+  useEffect(() => {
+    const rotateColors = () => {
+      anime({
+        targets: ".MenuBackground",
+        backgroundColor: randomColor(),
+        duration: 2000,
+        easing: "linear",
+        complete: rotateColors,
+      });
+    };
+    rotateColors();
+  }, []);
+
   return (
     <div className="Menu">
+      <div className="MenuBackground"></div>
       <img src={profilePic} alt="Isaac drinking coffee" />
       <MenuButton>About Me</MenuButton>
       <MenuButton>Projects</MenuButton>
