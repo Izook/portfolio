0c03745c62b5a74f7cf91d45903548f3f0eb16a3
Isaac Ortega <isaac.io.ortega@gmail.com>
2020-09-30 22:31:05 -0700

    Remove images, add projects list, begin work on Projects carousel

---
+++ src/components/Carousel/Carousel.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./Carousel.css";
+
+const Carousel = ({ children }) => {
+  return <div className="Carousel">{children}</div>;
+};
+
+export default Carousel;
--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -3,8 +3,11 @@ import React from "react";
 import "./Projects.css";
 
 import PageBaseComponent from "../../components/PageBaseComponent/PageBaseComponent";
+import Carousel from "../../components/Carousel/Carousel";
 import ModalContainer from "../../components/Modals/ModalContainer";
 import Modal from "../../components/Modals/Modal";
+
+import projects from "../../lib/projectsList";
 
 const Projects = () => {
   return (
@@ -12,6 +15,17 @@ const Projects = () => {
       <ModalContainer>
         <Modal isGhost="true" isBackless="true">
           <p>Here are some of my latest projects:</p>
+          <Carousel>
+            {projects.map((project) => (
+              <div key={project.name}>
+                <img alt={project.imageAlt} src={project.imagePath} />
+                <h1>{project.name}</h1>
+                <section
+                  dangerouslySetInnerHTML={{ __html: project.descriptionHTML }}
+                ></section>
+              </div>
+            ))}
+          </Carousel>
         </Modal>
       </ModalContainer>
     </PageBaseComponent>
