e6b5d85d5440240e0fb2cc341391d865d65ee53f
Izook <Izook@users.noreply.github.com>
2020-10-10 06:23:30 +0000

    Deploying to gh-pages from  @ 279ac5cd09e24952b03ee9423fa201ae3cf76ac4 🚀

---
+++ precache-manifest.3720cc343eb1bee83dcb26883213331a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d31fcf70ac8ea97b90bd4e38d080456",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6288e1bf8378f2999931",
+    "url": "/static/css/main.249daa6b.chunk.css"
+  },
+  {
+    "revision": "77c72b2da7fdbccd48d2",
+    "url": "/static/js/2.5259fe09.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5259fe09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6288e1bf8378f2999931",
+    "url": "/static/js/main.08d8b5f3.chunk.js"
+  },
+  {
+    "revision": "67cccf1e62b5641334fa",
+    "url": "/static/js/runtime-main.a8781de1.js"
+  },
+  {
+    "revision": "3f85ae9f225bdeb26c97a3c7f1eb5128",
+    "url": "/static/media/email.3f85ae9f.png"
+  },
+  {
+    "revision": "b7bf7046e1055517d969b32f737a5675",
+    "url": "/static/media/phone.b7bf7046.png"
+  },
+  {
+    "revision": "d7fbabb65a521b5fc8d153c69982a328",
+    "url": "/static/media/profile.d7fbabb6.jpg"
+  },
+  {
+    "revision": "7542c3a308832561528c01ff4f60969d",
+    "url": "/static/media/skull.7542c3a3.svg"
+  }
+]);
