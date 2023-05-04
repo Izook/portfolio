20a6b76a553c99ec804ef7da49b8db38f1373a4b
Isaac Ortega <isaac.io.ortega@gmail.com>
2023-05-04 00:09:12 -0700

    feat: remove twitter social icon

--- src/components/ContactIcons/ContactIcons.js
+++ src/components/ContactIcons/ContactIcons.js
@@ -6,16 +6,10 @@ import "./ContactIcons.css";
 import githubIcon from "../../assets/github.png";
 import linkedinIcon from "../../assets/linkedin.png";
 import itchIcon from "../../assets/itchio.png";
-import twitterIcon from "../../assets/twitter.png";
 
 const ContactIcons = () => {
   return (
     <div className="ContactIcons">
-      <ContactIcon
-        icon={twitterIcon}
-        url="https://twitter.com/izookojay"
-        altText="Link to my twitter page"
-      />
       <ContactIcon
         icon={itchIcon}
         url="https://izook.itch.io/"
